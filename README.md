c1d4d83462563ecfc4ce5f90e6ea254df5632807
yangliulnn <yangliulnn@163.com>
2017-07-31 18:22:15 +0800

    💥 big bang

---
+++ README.md
@@ -0,0 +1,33 @@
+# IDCard
+身份证号校验及信息获取
+
+# 使用
+## 安装
+```
+$ composer require leonis/id-card
+```
+
+## 用法
+```php
+$idCard = new \Leonis\IDCard\IDCard($idCardNumber);
+
+$idCard->check();           // 验证身份号 return true or false
+$idCard->checkAreaCode();   // 验证行政区划代码 return true or false
+$idCard->checkBirthday();   // 验证生日 return true or false
+$idCard->checkCode();       // 验证校验码 return true or false
+$idCard->address();         // 获取地址 return string or false
+$idCard->province();        // 获取省 return string or false
+$idCard->city();            // 获取市 return string or false
+$idCard->zone();            // 获取区 return string or false
+$idCard->birthday();        // 获取生日 return string or false
+$idCard->year();            // 获取年 return int or false
+$idCard->month();           // 获取月 return int or false
+$idCard->day();             // 获取日 return int or false
+$idCard->age();             // 获取年龄 return int or false
+$idCard->sex();             // 获取性别 return string or false
+$idCard->constellation();   // 获取星座 return string or false
+$idCard->zodiac();          // 获取属相 return string or false
+```
+ 
+## License
+MIT
---
+++ src/ID.php
@@ -0,0 +1,289 @@
+<?php
+
+namespace Leonis\IDCard;
+
+class IDCard
+{
+    /**
+     * 身份证号
+     *
+     * @var string
+     */
+    private $id;
+
+    /**
+     * 行政区划代码
+     *
+     * @var array
+     */
+    private $areaCodes;
+
+    public function __construct(string $id)
+    {
+        $this->id        = strtoupper($id);
+        $this->areaCodes = (array)require 'C:\Users\lN\Desktop\lianbi-web\vendor\leonis\id\data\codes.php';
+    }
+
+    /**
+     * 验证身份号
+     *
+     * @return bool
+     */
+    public function check()
+    {
+        return ($this->checkAreaCode() and $this->checkBirthday() and $this->checkCode()) ? true : false;
+    }
+
+    /**
+     * 验证行政区划代码
+     *
+     * @return bool
+     */
+    public function checkAreaCode()
+    {
+        $areaCode = substr($this->id, 0, 6);
+
+        return array_key_exists($areaCode, $this->areaCodes);
+    }
+
+    /**
+     * 验证生日
+     *
+     * @return bool
+     */
+    public function checkBirthday()
+    {
+        $year  = substr($this->id, 6, 4);
+        $month = substr($this->id, 10, 2);
+        $day   = substr($this->id, 12, 2);
+
+        return checkdate($month, $day, $year);
+    }
+
+    /**
+     * 验证校验码
+     *
+     * @return bool
+     */
+    public function checkCode()
+    {
+        $weight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+        $codes  = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+        $code   = substr($this->id, 17, 1);
+        $sum    = 0;
+        for ($i = 0; $i < 17; $i++) {
+            $sum += substr(substr($this->id, 0, 17), $i, 1) * $weight[$i];
+        }
+
+        return $code == $codes[$sum % 11];
+    }
+
+    /**
+     * 获取地址
+     *
+     * @param string $separator
+     * @return bool|string
+     */
+    public function address(string $separator = '')
+    {
+        return $this->check() ? $this->province() . $separator . $this->city() . $separator . $this->zone() : false;
+    }
+
+    /**
+     * 获取省
+     *
+     * @return bool|mixed
+     */
+    public function province()
+    {
+        $provinceCode = substr($this->id, 0, 2) . '0000';
+
+        return $this->check() ? $this->areaCodes[$provinceCode] : false;
+    }
+
+    /**
+     * 获取市
+     *
+     * @return bool|mixed
+     */
+    public function city()
+    {
+        $cityCode = substr($this->id, 0, 4) . '00';
+
+        return $this->check() ? $this->areaCodes[$cityCode] : false;
+    }
+
+    /**
+     * 获取区
+     *
+     * @return bool|mixed
+     */
+    public function zone()
+    {
+        $areaCode = substr($this->id, 0, 6);
+
+        return $this->check() ? $this->areaCodes[$areaCode] : false;
+    }
+
+    /**
+     * 获取生日
+     *
+     * @param string $format
+     * @return bool|string
+     */
+    public function birthday(string $format)
+    {
+        return date($format, strtotime($this->year() . '-' . $this->month() . '-' . $this->day()));
+    }
+
+    /**
+     * 获取年
+     *
+     * @return int
+     */
+    public function year()
+    {
+        return (int)substr($this->id, 6, 4);
+    }
+
+    /**
+     * 获取月
+     *
+     * @return int
+     */
+    public function month()
+    {
+        return (int)substr($this->id, 10, 2);
+    }
+
+    /**
+     * 获取日
+     *
+     * @return int
+     */
+    public function day()
+    {
+        return (int)substr($this->id, 12, 2);
+    }
+
+    /**
+     * 获取年龄
+     *
+     * @return false|int|string
+     */
+    public function age()
+    {
+        if (! $this->check()) {
+            return false;
+        }
+
+        $year  = substr($this->id, 6, 4);
+        $month = substr($this->id, 10, 12);
+        $day   = substr($this->id, 12, 14);
+
+        $age = 0;
+        if (date('Y') > $year) {
+            $age = date('Y') - $year - 1;
+            if (date('m') > $month) {
+                $age++;
+            } elseif (date('m') == $month) {
+                if (date('d') >= $day) {
+                    $age++;
+                }
+            }
+        }
+
+        return $age;
+    }
+
+    /**
+     * 获取性别
+     * @return bool|string 身份证号未通过验证返回 false
+     */
+    public function sex()
+    {
+        return $this->check() ? (substr($this->id, 16, 1) % 2 === 0 ? '女' : '男') : false;
+    }
+
+    /**
+     * 获取星座
+     *
+     * @return bool|mixed
+     */
+    public function constellation()
+    {
+        if (! $this->check()) {
+            return false;
+        }
+
+        $constellation     = ['水瓶座', '双鱼座', '白羊座', '金牛座', '双子座', '巨蟹座', '狮子座', '处女座', '天秤座', '天蝎座', '射手座', '魔羯座'];
+        $constellationDays = [21, 20, 21, 20, 21, 22, 23, 23, 23, 24, 22, 21];
+
+        $month = $this->month() - 1;
+        $day   = $this->day();
+
+        if ($day < $constellationDays[$month - 1]) {
+            $month--;
+        }
+
+        if ($month > 0) {
+            return $constellation[$month];
+        }
+
+        return $constellation[11];
+    }
+
+    /**
+     * 获取属相
+     *
+     * @return bool|string
+     */
+    public function zodiac()
+    {
+        if (! $this->check()) {
+            return false;
+        }
+
+        $year  = $this->year();
+        $index = $year > 1901 ? ($year - 1901) % 12 : (1901 - $year) % 12;
+
+        switch ($index) {
+            case 0:
+                return '牛';
+                break;
+            case 1:
+                return '虎';
+                break;
+            case 2:
+                return '兔';
+                break;
+            case 3:
+                return '龙';
+                break;
+            case 4:
+                return '蛇';
+                break;
+            case 5:
+                return '马';
+                break;
+            case 6:
+                return '羊';
+                break;
+            case 7:
+                return '猴';
+                break;
+            case 8:
+                return '鸡';
+                break;
+            case 9:
+                return '狗';
+                break;
+            case 10:
+                return '猪';
+                break;
+            case 11:
+                return '鼠';
+                break;
+        }
+    }
+}
